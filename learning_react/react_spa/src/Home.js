71b87c260a0f7b3d1ace612dfe44caa289c8a548
Seth A. Johnson <sethajoh@umich.edu>
2019-04-02 15:27:18 -0400

    chapter 18 react-router

---
+++ learning_react/react_spa/src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>hello</h2>
+        <p>stuff here about home</p>
+        <p>more home stuff</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
